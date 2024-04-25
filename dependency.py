db262f1b4f1ecfa517990c0e026e188349113177
avisek12 <109167023+avisek12@users.noreply.github.com>
2024-04-25 11:16:23 +0530

    Create dependency.py

---
+++ dependency.py
@@ -0,0 +1,21 @@
+organization=[
+         #"paresh",
+         #"roshni",
+         #"TFE-DEV",
+         "TFE-PROD-NEW"
+         ]
+github_repo=[
+       #"TFE-NONPROD",
+       #"TFE-DEV",
+       #"TFE-PROD-NEW",
+       #"TFE-PROD-NEW1",
+       "TFE-PROD-NEW2"
+       ]
+workspace=[
+    #"TFE-PROD-APP1",
+    #"TFE-PROD-APP2",
+    #"TFE-PROD-APP3",
+    "TFE-PROD-APP4"
+    ]
+githubuser="avisek12"
+
